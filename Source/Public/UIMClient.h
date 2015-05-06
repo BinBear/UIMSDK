57143a92e253f4b7f831130170b40b579d5bd4a9
Retso Huang <retsohuang@gmail.com>
2015-05-06 15:17:04 +0800

    Remove module import

--- Source/Public/UIMClient.h
+++ Source/Public/UIMClient.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014 Devpro. All rights reserved.
 //
 
-@import Foundation;
-@import UIKit;
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol UIMClientDelegate;
 
